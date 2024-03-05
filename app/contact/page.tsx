12b071b90ba3222ffa43ec033256d2d0baa546d3
Vitaliy Bychik <vitaliy.bychik@gmail.com>
2024-03-05 14:13:26 +0000

    finished basic Home page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -13,62 +13,63 @@ export default function Contact() {
       <div className="w-screen flex flex-col text-white text-2xl items-center px-10 md:pt-20">
         <div className="max-w-4xl space-y-20">
 
-        {/* block 1 */}
-        <div className="flex flex-row">
-          <div className="flex flex-col">
-            <div className="text-center sm:text-left text-2xl md:text-4xl mb-6">
-              Let&apos;s make something great together!
-            </div>
-            <div className="leading-loose md:leading-normal 
+          {/* block 1 */}
+          <div className="flex flex-row">
+            <div className="flex flex-col">
+              <div className="text-center sm:text-left text-2xl md:text-4xl mb-6">
+                Let&apos;s make something great together!
+              </div>
+              <div className="leading-loose md:leading-normal 
               text-base md:text-lg lg:text-xl xl:text-2xl text-pretty text-zinc-300">
-              Just drop me a message to
-              <div className="block sm:ml-2 sm:inline">
-                <Link
-                  href={`mailto:${email}`}
-                  target="_blank"
-                  className="duration-200 hover:text-white px-2 py-1 mr-2 
+                Just drop me a message to
+                <div className="block sm:ml-2 sm:inline">
+                  <Link
+                    href={`mailto:${email}`}
+                    target="_blank"
+                    className="duration-200 hover:text-white px-2 py-1 mr-2 
                   border rounded-lg border-zinc-300 hover:border-white
                   hover:shadow-[0_0_5px] hover:shadow-white"
-                >
-                  <FontAwesomeIcon icon={faEnvelope} /> {email}
-                </Link>
-                or via&nbsp;
-                <Link
-                  href="https://www.linkedin.com/in/vitaliy-bychik-18163558/"
-                  target="_blank"
-                  className="duration-200 hover:text-white px-2 py-1 mr-2 
+                  >
+                    <FontAwesomeIcon icon={faEnvelope} /> {email}
+                  </Link>
+                  or via
+                </div>
+                <div className="block sm:ml-2 sm:inline">
+                  <Link
+                    href="https://www.linkedin.com/in/vitaliy-bychik-18163558/"
+                    target="_blank"
+                    className="duration-200 hover:text-white px-2 py-1 mr-2 
                   border rounded-lg border-zinc-300 hover:border-white
                   hover:shadow-[0_0_5px] hover:shadow-white"
-                >
-                  <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
-                </Link>
+                  >
+                    <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+                  </Link>
+                </div>
 
               </div>
-              
             </div>
           </div>
-        </div>
 
-        {/* block 2 */}
-        <div className="flex flex-col sm:flex-row">
-          <div className="flex basis-1/2  mb-2">
-            <p className="text-base md:text-lg lg:text-xl xl:text-2xl text-pretty text-zinc-300">
-              I&apos;m located in Lisbon, Portugal. My working hours are
-              extremely flexible, having solid intersection with all the
-              time zones in the U.S. and Europe.
-            </p>
+          {/* block 2 */}
+          <div className="flex flex-col sm:flex-row">
+            <div className="flex basis-1/2  mb-2">
+              <p className="text-base md:text-lg lg:text-xl xl:text-2xl text-pretty text-zinc-300">
+                I&apos;m located in Lisbon, Portugal. My working hours are
+                extremely flexible, having solid intersection with all the
+                time zones in the U.S. and Europe.
+              </p>
+            </div>
+            <div className="flex basis-1/2 justify-center">
+              <Image
+                src="/white-dotted-map-pt.png"
+                alt="Vitaliy's Profile Image"
+                width={410}
+                height={241}
+                className=""
+              />
+            </div>
           </div>
-          <div className="flex basis-1/2 justify-center">
-            <Image
-              src="/white-dotted-map-pt.png"
-              alt="Vitaliy's Profile Image"
-              width={410}
-              height={241}
-              className=""
-            />
-          </div>
-        </div>
-        
+
         </div>
       </div>
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
+import Image from 'next/image';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
 
 export default function Home() {
   return (
     <main>
+      <div className="w-screen px-10">
 
-      <div className="w-screen h-screen flex flex-col sm:flex-row items-center justify-center border-2 text-white text-2xl space-y-2">
+      <div className="h-[90vh] flex flex-col sm:flex-row items-center justify-center text-white text-2xl space-y-2">
         <div className="flex flex-col mb-2">
           <Image
             src="/profile-pic2.png"
@@ -23,11 +27,32 @@ export default function Home() {
           </p>
         </div>
       </div>
-      <div className="w-screen h-screen flex items-center justify-center border-2 text-white text-2xl">
-        The one-man tech team poised to make your next great idea a reality.
+      <div className="h-screen flex items-center justify-center text-white text-2xl">
+        <div className="max-w-4xl space-y-10">
+          <p className="text-center sm:text-left text-base md:text-lg lg:text-xl xl:text-2xl text-wrap">
+            I&apos;ve held many roles in my career, from a junior software engineer to the head of the development department. 
+          </p>
+          <p className="text-center sm:text-left text-base md:text-lg lg:text-xl xl:text-2xl text-wrap">
+            Currently I&apos;m obsessed with front-end - Next.js, React, Tailwind CSS, and Vercel.
+          </p>
+          <p className="text-center sm:text-left text-base md:text-lg lg:text-xl xl:text-2xl text-wrap">
+            Got a project and need someone to help out? I&apos;m your guy!
+          </p>
+          <div>
+            <Link
+              href="/contact"
+              className="duration-200 hover:text-white px-2 py-1 mr-2 
+                  border rounded-lg border-zinc-300 hover:border-white
+                  hover:shadow-[0_0_5px] hover:shadow-white"
+            >
+              <FontAwesomeIcon icon={faPaperPlane} /> Get in touch
+            </Link>
+
+          </div>
+        </div>
       </div>
 
-      
+      </div>
       
     </main>
   );
